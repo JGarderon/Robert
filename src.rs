4b3ac051e566b8086d1d392e63fe8822b180bebd
Nothus <julien.garderon@gmail.com>
2020-04-07 17:35:30 +0200

    Update src.rs

--- src.rs
+++ src.rs
@@ -16,6 +16,8 @@ use std::sync::mpsc::{Sender, Receiver};
 
 const DEBUG: bool = true; 
 
+const DICO_NOM_DEFAUT: &'static str = "défaut";
+
 const TAILLE_LIGNE_MAX: usize = 1024; 
 const TAILLE_TEXTE_MAX: usize = TAILLE_LIGNE_MAX*5; 
 
@@ -61,6 +63,7 @@ impl Retour {
 enum Valeurs { 
 	Boolean(bool), 
 	Entier(u32), 
+	Flottant(f32), 
 	Texte(String) 
 } 
 
@@ -78,7 +81,11 @@ impl Valeurs {
 			"booléen" => match self { 
 				Valeurs::Boolean( _ ) => true, 
 				Valeurs::Entier( n ) => {
-					*self = Valeurs::Boolean( if *n > 0 { true } else { false } ); 
+					*self = Valeurs::Boolean( if *n > 0u32 { true } else { false } ); 
+					true 
+				} 
+				Valeurs::Flottant( n ) => { 
+					*self = Valeurs::Boolean( if *n > 0f32 { true } else { false } ); 
 					true 
 				} 
 				Valeurs::Texte( t ) => { 
@@ -100,10 +107,33 @@ impl Valeurs {
 				Valeurs::Boolean( b ) => { 
 					*self = Valeurs::Entier( if *b { 1 } else { 0 } ); 
 					true 
-				}, 
+				} 
+				Valeurs::Flottant( n ) => { 
+					*self = Valeurs::Entier( n.round() as u32 ); 
+					true 
+				} 
 				Valeurs::Texte( t ) => { 
 					if let Ok( n ) = t.parse::<u32>() { 
 						*self = Valeurs::Entier( n ); 
+						true 
+					} else { 
+						false 
+					} 
+				} 
+			} 
+			"flottant" => match self { 
+				Valeurs::Flottant( _ ) => true, 
+				Valeurs::Entier( n ) => { 
+					*self = Valeurs::Flottant( f32::from_bits( *n ) ); 
+					true 
+				}, 
+				Valeurs::Boolean( b ) => { 
+					*self = Valeurs::Flottant( if *b { 1f32 } else { 0f32 } ); 
+					true 
+				}, 
+				Valeurs::Texte( t ) => { 
+					if let Ok( n ) = t.parse::<f32>() { 
+						*self = Valeurs::Flottant( n ); 
 						true 
 					} else { 
 						false 
@@ -117,6 +147,10 @@ impl Valeurs {
 					true 
 				}, 
 				Valeurs::Entier( n ) => { 
+					*self = Valeurs::Texte( n.to_string() ); 
+					true 
+				} 
+				Valeurs::Flottant( n ) => { 
 					*self = Valeurs::Texte( n.to_string() ); 
 					true 
 				} 
@@ -177,9 +211,10 @@ fn resoudre_obtenir( contexte: &mut Contexte, arguments: &str ) -> Retour {
 	let valeurs = &dico.liste; 
 	if valeurs.contains_key( arguments.trim() ) { 
 		match &valeurs[arguments] { 
-			Valeurs::Boolean( b ) => Retour::creer( true, format!( "{}", b ) ), 
-			Valeurs::Texte( t ) => Retour::creer( true, format!( "\"{}\"", t ) ), 
-			Valeurs::Entier( n ) => Retour::creer( true, format!( "{}", n ) ), 
+			Valeurs::Boolean( b ) => Retour::creer( true, format!( "(booléen) {}", b ) ), 
+			Valeurs::Texte( t ) => Retour::creer( true, format!( "(texte) \"{}\"", t ) ), 
+			Valeurs::Entier( n ) => Retour::creer( true, format!( "(entier) {}", n ) ), 
+			Valeurs::Flottant( n ) => Retour::creer( true, format!( "(flottant) {}", n ) ), 
 		} 
 	} else { 
 		Retour::creer_str( false, "clé inconnue" ) 
@@ -255,7 +290,7 @@ fn resoudre_alterer( contexte: &mut Contexte, arguments: &str ) -> Retour {
 		let r#type: &str = &arguments[position+1..]; 
 		if cle != "" { 
 			return match r#type { 
-				"booléen" | "texte" | "entier" => { 
+				"booléen" | "texte" | "entier" | "flottant" => { 
 					if let Some( v ) = valeurs.get_mut( cle ) { 
 						if v.alterer( r#type ) { 
 							Retour::creer_str( true, "altération effectuée" ) 
@@ -282,8 +317,26 @@ fn resoudre_incrementer( contexte: &mut Contexte, arguments: &str ) -> Retour {
 	if let Some( v ) = valeurs.get_mut( arguments.trim() ) { 
 		match v { 
 			Valeurs::Entier( n ) => { 
-				*n += 1; 
-				Retour::creer_str( true, "incrémentation effectuée" ) 	
+				// if arguments.trim() == "" { 
+					*n += 1; 
+					Retour::creer_str( true, "incrémentation (+1) effectuée" ) 
+				// } else if let Ok( m ) = arguments.trim().parse::<u32>() { 
+				// 	*n += m; 
+				// 	Retour::creer_str( true, "incrémentation arbitraire effectuée" ) 	
+				// } else { 
+				// 	Retour::creer_str( false, "incrémentation impossible, l'argument est invalide dans ce type" ) 
+				// } 
+			} 
+			Valeurs::Flottant( n ) => { 
+				// if arguments.trim() == "" { 
+					*n += 1f32; 
+					Retour::creer_str( true, "incrémentation (+1) effectuée" ) 
+				// } else if let Ok( m ) = arguments.trim().parse::<f32>() { 
+				// 	*n += m; 
+				// 	Retour::creer_str( true, "incrémentation arbitraire effectuée" ) 	
+				// } else { 
+				// 	Retour::creer_str( false, "incrémentation impossible, l'argument est invalide dans ce type" ) 
+				// } 
 			} 
 			_ => Retour::creer_str( false, "incrémentation impossible" ) 
 		} 
@@ -354,8 +407,8 @@ fn resoudre_canal_supprimer( contexte: &mut Contexte, arguments: &str ) -> Retou
 		Retour::creer_str( false, "nom de canal obligatoire" ) 
 	} else if nom.len() > 32 { 
 		Retour::creer_str( false, "nom de canal trop long (max 32)" ) 
-	} else if nom == "défaut" { 
-		Retour::creer_str( false, "impossible de supprimer le canal \"défaut\"" ) 
+	} else if nom == DICO_NOM_DEFAUT { 
+		Retour::creer_str( false, "impossible de supprimer le canal par défaut" ) 
 	} else { 
 		let mut dicos = contexte.dicos.lock().unwrap(); 
 		if dicos.liste.contains_key( nom ) { 
@@ -512,7 +565,7 @@ fn resoudre( contexte: &mut Contexte, appel: &str, arguments: &str ) -> Retour {
 			"canal:capturer" => resoudre_canal_capture, 
 			"canal:supprimer" => resoudre_canal_supprimer, 
 			"canal:tester" => resoudre_canal_tester, 
-			"canal:lister" => resoudre_canal_lister, 
+			"canal:lister" if DEBUG => resoudre_canal_lister, 
 			"canal:changer" => resoudre_canal_changer, 
 			"canal:souscrire" => resoudre_canal_souscrire, 
 			"canal:émettre" => resoudre_canal_emettre, 
@@ -655,13 +708,11 @@ struct Dictionnaires {
 	liste: HashMap<String,DictionnaireThread> 
 } 
 
-// ---------------------------------------------------- 
-
-fn main() -> std::io::Result<()> { 
+fn creer_racine( nom_defaut: &str ) -> (DictionnaireThread, Arc<Mutex<Dictionnaires>>) { 
 	let mut tmp = Dictionnaires { 
 		liste: HashMap::new() 
 	}; 
-	let nom = "défaut".to_string(); 
+	let nom = nom_defaut.to_string(); 
 	let dico_thread = Arc::new( 
 		Mutex::new( 
 			Dictionnaire { 
@@ -680,9 +731,25 @@ fn main() -> std::io::Result<()> {
 			tmp 
 		) 
 	); 
+	( 
+		dico_thread, 
+		dicos 
+	) 
+} 
+
+// fn lancement_service( ipport: &str ) -> Result<JoinHandle> { 
+
+// } 
+
+// ---------------------------------------------------- 
+
+fn main() -> std::io::Result<()> { 
+
+	let (dico_thread, dicos) = creer_racine( DICO_NOM_DEFAUT ); 
+
     let listener = TcpListener::bind("127.0.0.1:8080")?; 
     let mut fils: Vec<JoinHandle<_>> = Vec::new(); 
-    for resultat in listener.incoming() {
+    for resultat in listener.incoming() { 
     	match resultat { 
     		Ok( stream ) => { 
     			if DEBUG { 
