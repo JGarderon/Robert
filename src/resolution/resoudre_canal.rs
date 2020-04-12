e630275df841a81e6e3daa8414eaa3f0951bda8b
Julien Garderon <julien.garderon@gmail.com>
2020-04-12 21:40:54 +0200

    fin de l'implémentation minimale des canaux, avec sousciption complète

--- src/resolution/resoudre_canal.rs
+++ src/resolution/resoudre_canal.rs
@@ -21,8 +21,6 @@ use crate::resolution::Retour;
 
 // ---------------------------------------------------- 
 
-use crate::DEBUG; 
-use crate::CANAL_NOM_DEFAUT; 
 use crate::NBRE_MAX_CANAUX; 
 
 // ---------------------------------------------------- 
@@ -65,7 +63,7 @@ fn resoudre_creer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> 
 /// # Fonction de résolution locale "supprimer un canal existant" 
 ///
 /// Cette fonction tentera de supprimer un canal existant mais auparavant, elle avertira tous les éventuels souscripteurs présents de cette action, et les retira de la liste des souscripteurs. 
-///
+/// 
 fn resoudre_supprimer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> Retour { 
 	let nom = if let Some( n ) = arguments.extraire() { 
 		n 
@@ -100,7 +98,7 @@ fn resoudre_supprimer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux )
 		} else { 
 			Retour::creer_str( false, "canal inexistant" ) 
 		} 
-	}
+	} 
 } 
 
 /// # Fonction de résolution locale "tester l'existence d'un canal" 
@@ -151,69 +149,114 @@ fn resoudre_capturer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) 
 	Retour::creer_str( true, "canal privé actif" ) 
 } 
 
-// fn resoudre_changer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> Retour { 
-// 	let nom = if let Some( n ) = arguments.extraire() { 
-// 		n 
-// 	} else { 
-// 		return Retour::creer_str( false, "nom de canal obligatoire" ); 
-// 	}; 
-// 	if nom.len() > 32 { 
-// 		Retour::creer_str( false, "nom de canal trop long (max. 32)" ) 
-// 	} else { 
-// 		let dicos = contexte.dicos.lock().unwrap(); 
-// 		if dicos.liste.contains_key( &nom ) { 
-// 			contexte.dico = dicos.liste[&nom].clone(); 
-// 			Retour::creer_str( true, "canal modifié" ) 
-// 		} else { 
-// 			Retour::creer_str( false, "canal inexistant" ) 
-// 		} 
-// 	} 
-// } 
+/// # Fonction de résolution locale "renommer un canal existant" 
+///
+/// Cette fonction repose en grande partie sur sa soeur de suppression d'un canal existant. La raison repose sur le fonctionnement interne de Rust : "_It is a logic error for a key to be modified in such a way that the key's hash, as determined by the Hash trait, or its equality, as determined by the Eq trait, changes while it is in the map. This is normally only possible through Cell, RefCell, global state, I/O, or unsafe code._" (voir : https://doc.rust-lang.org/std/collections/struct.HashMap.html) 
+/// 
+/// Robert évitant à tout prix le code non-sûr et vu la complexité introduite avec les souscripteurs, il est plus aisé qu'un renommage produise l'effet d'une suppression et la réintroduction du canal avec son nouveau nom. 
+/// 
+fn resoudre_renommer( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> Retour { 
+	let ancien_nom = if let Some( n ) = arguments.extraire() { 
+		n 
+	} else { 
+		return Retour::creer_str( false, "ancien nom de canal obligatoire" ); 
+	}; 
+	let nouveau_nom = if let Some( n ) = arguments.extraire() { 
+		n 
+	} else { 
+		return Retour::creer_str( false, "nouveau nom de canal obligatoire" ); 
+	}; 
+	if ancien_nom.len() > 32 || ancien_nom.len() > 32 { 
+		Retour::creer_str( false, "l'un des deux noms est trop long (max. 32)" ) 
+	} else { 
+		let mut canaux = match contexte.canauxthread.lock() { 
+			Ok( c ) => c, 
+			Err( e ) => e.into_inner() 
+		}; 
+		if canaux.liste.contains_key( &nouveau_nom ) { 
+			return Retour::creer_str( false, "nouveau nom de canal déjà existant" ); 
+		} 
+		if canaux.liste.contains_key( &ancien_nom ) { 
+			{ 
+				let message = "canal modifié".to_string(); 
+				match canaux.liste[&ancien_nom].lock() { 
+					Ok( c ) => c, 
+					Err( e ) => e.into_inner() 
+				}.souscripteurs.retain( 
+					| souscripteur | { 
+						souscripteur.send( message.clone() ).unwrap(); 
+						false 
+					} 
+				); 
+			} 
+			if let Some( canal ) = canaux.liste.remove( &ancien_nom ) { 
+				match canal.lock() { 
+					Ok( c ) => c, 
+					Err( e ) => e.into_inner() 
+				}.nom = nouveau_nom.to_string(); 
+				canaux.liste.insert( 
+					nouveau_nom, 
+					canal 
+				); 
+				Retour::creer_str( true, "canal supprimé" ) 
+			} else { 
+				Retour::creer_str( false, "impossible de supprimer le canal" ) 
+			} 
+		} else { 
+			Retour::creer_str( false, "canal inexistant" ) 
+		} 
+	} 
+} 
 
-// fn resoudre_souscrire( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> Retour { 
-// 	if let Some( _ ) = arguments.extraire() { 
-// 		return Retour::creer_str( false, "aucun argument accepté pour cette fonction" ); 
-// 	} 
-// 	let (expediteur, destinaire): ( Sender<String>, Receiver<String> ) = mpsc::channel(); 
-// 	{ 
-// 		let mut dico = contexte.dico.lock().unwrap(); 
-// 		dico.souscripteurs.push( expediteur ); 
-// 	} 
-// 	while let Ok( m ) = destinaire.recv() { 
-// 		if let Ok( _ ) = contexte.stream.write( 
-// 			format!( "\t>>> {}\n", m ).as_bytes() 
-// 		) { 
-// 			if let Ok( _ ) = contexte.stream.flush() { 
-// 			} else { 
-// 				break; 
-// 			} 
-// 		} else { 
-// 			break; 
-// 		}  
-// 	} 
-// 	Retour::creer_str( true, "diffusion terminée" ) 
-// } 
+fn resoudre_souscrire( contexte: &mut Contexte, mut arguments: ArgumentsLocaux ) -> Retour { 
+	if let Some( _ ) = arguments.extraire() { 
+		return Retour::creer_str( false, "aucun argument accepté pour cette fonction" ); 
+	} 
+	let (expediteur, destinaire): ( Sender<String>, Receiver<String> ) = mpsc::channel(); 
+	{ 
+		match contexte.canalthread.lock() { 
+			Ok( c ) => c, 
+			Err( e ) => e.into_inner() 
+		}.souscripteurs.push( expediteur ); 
+	} 
+	while let Ok( m ) = destinaire.recv() { 
+		if let Ok( _ ) = contexte.stream.write( 
+			format!( "\t>>> {}\n", m ).as_bytes() 
+		) { 
+			if let Ok( _ ) = contexte.stream.flush() { 
+			} else { 
+				break; 
+			} 
+		} else { 
+			break; 
+		}  
+	} 
+	Retour::creer_str( true, "diffusion terminée" ) 
+} 
 
-// fn resoudre_emettre( contexte: &mut Contexte, arguments: ArgumentsLocaux ) -> Retour { 
-// 	let mut dico = contexte.dico.lock().unwrap(); 
-// 	let message = arguments.source.iter().collect::<String>(); 
-// 	dico.souscripteurs.retain( 
-// 		| souscripteur | { 
-// 			if let Ok( _ ) = souscripteur.send( message.clone() ) { 
-// 				true 
-// 			} else { 
-// 				false 
-// 			} 
-// 		} 
-// 	); 
-// 	Retour::creer( 
-// 		true, 
-// 		format!( 
-// 			"diffusion émise aux souscripteurs ({})", 
-// 			dico.souscripteurs.len() 
-// 		) 
-// 	) 
-// } 
+fn resoudre_emettre( contexte: &mut Contexte, arguments: ArgumentsLocaux ) -> Retour { 
+	let message = arguments.source.iter().collect::<String>(); 
+	let mut c = match contexte.canalthread.lock() { 
+		Ok( c ) => c, 
+		Err( e ) => e.into_inner() 
+	}; 
+	c.souscripteurs.retain( 
+		| souscripteur | { 
+			if let Ok( _ ) = souscripteur.send( message.clone() ) { 
+				true 
+			} else { 
+				false 
+			} 
+		} 
+	); 
+	Retour::creer( 
+		true, 
+		format!( 
+			"diffusion émise aux souscripteurs ({})", 
+			c.souscripteurs.len() 
+		) 
+	) 
+} 
 
 pub fn resoudre( appel: &str ) -> Result<Resolveur,Retour> { 
 	match appel { 
@@ -221,9 +264,9 @@ pub fn resoudre( appel: &str ) -> Result<Resolveur,Retour> {
 		"supprimer" => Ok( resoudre_supprimer as Resolveur ), 
 		"tester" => Ok( resoudre_tester as Resolveur ), 
 		"capturer" => Ok( resoudre_capturer as Resolveur ), 
-		// "changer" => Ok( resoudre_changer as Resolveur ), 
-		// "souscrire" => Ok( resoudre_souscrire as Resolveur ), 
-		// "emettre" => Ok( resoudre_emettre as Resolveur ), 
+		"renommer" => Ok( resoudre_renommer as Resolveur ), 
+		"souscrire" => Ok( resoudre_souscrire as Resolveur ), 
+		"emettre" => Ok( resoudre_emettre as Resolveur ), 
 		_ => Err( Retour::creer_str( false, "module texte : fonction inconnue" ) ) 
 	} 
 } 
